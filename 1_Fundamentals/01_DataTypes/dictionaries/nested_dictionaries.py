aaa7a6617cc74098a0a310844d3a9e7eec2cf0a8
Josh Hayles <josh@joshhayles.com>
2022-07-25 17:33:10 -0600

    Day 11: second commit

---
+++ 1_Fundamentals/01_DataTypes/dictionaries/nested_dictionaries.py
@@ -0,0 +1,15 @@
+
+myfamily = {
+    "child1" : {
+        "name" : "Emily",
+        "year" : 2004
+    },
+    "child2" : {
+        "name" : "Toby",
+        "year" : 2007
+    },
+    "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+    }
+}
